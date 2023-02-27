af1ba0ce584415ccdd3210d2c8bcec1d35d16e7b
Bailey Franklin <baileydfranklin@gmail.com>
2023-02-26 20:19:20 -0700

    Added files and the deck

---
+++ deck.js
@@ -0,0 +1,158 @@
+const deck = [
+    {
+        name: "AC", value: 1, suit: black // 0
+    },
+    {
+        name: "2C", value: 2, suit: black // 1
+    },
+    {
+        name: "3C", value: 3, suit: black // 2
+    },
+    {
+        name: "4C", value: 4, suit: black // 3
+    },
+    {
+        name: "5C", value: 5, suit: black // 4
+    },
+    {
+        name: "6C", value: 6, suit: black // 5
+    },
+    {
+        name: "7C", value: 7, suit: black // 6
+    },
+    {
+        name: "8C", value: 8, suit: black // 7
+    },
+    {
+        name: "9C", value: 9, suit: black // 8
+    },
+    {
+        name: "10C", value: 10, suit: black // 9
+    },
+    {
+        name: "JC", value: 11, suit: black // 10
+    },
+    {
+        name: "QC", value: 12, suit: black // 11
+    },
+    {
+        name: "KC", value: 13, suit: black // 12
+    },
+    {
+        name: "AD", value: 1, suit: red // 13
+    },
+    {
+        name: "2D", value: 2, suit: red // 14
+    },
+    {
+        name: "3D", value: 3, suit: red // 15
+    },
+    {
+        name: "4D", value: 4, suit: red // 16
+    },
+    {
+        name: "5D", value: 5, suit: red // 17
+    },
+    {
+        name: "6D", value: 6, suit: red // 18
+    },
+    {
+        name: "7D", value: 7, suit: red // 19
+    },
+    {
+        name: "8D", value: 8, suit: red // 20
+    },
+    {
+        name: "9D", value: 9, suit: red // 21
+    },
+    {
+        name: "10D", value: 10, suit: red // 22
+    },
+    {
+        name: "JD", value: 11, suit: red // 23
+    },
+    {
+        name: "QD", value: 12, suit: red // 24
+    },
+    {
+        name: "KD", value: 13, suit: red // 25
+    },
+    {
+        name: "AH", value: 1, suit: red // 26
+    },
+    {
+        name: "2H", value: 2, suit: red // 27
+    },
+    {
+        name: "3H", value: 3, suit: red // 28
+    },
+    {
+        name: "4H", value: 4, suit: red // 29
+    },
+    {
+        name: "5H", value: 5, suit: red // 30
+    },
+    {
+        name: "6H", value: 6, suit: red // 31
+    },
+    {
+        name: "7H", value: 7, suit: red // 32
+    },
+    {
+        name: "8H", value: 8, suit: red // 33
+    },
+    {
+        name: "9H", value: 9, suit: red // 34
+    },
+    {
+        name: "10H", value: 10, suit: red // 35
+    },
+    {
+        name: "JH", value: 11, suit: red // 36
+    },
+    {
+        name: "QH", value: 12, suit: red // 37
+    },
+    {
+        name: "KH", value: 13, suit: red // 38
+    },
+    {
+        name: "AS", value: 1, suit: black // 39
+    },
+    {
+        name: "2S", value: 2, suit: black // 40
+    },
+    {
+        name: "3S", value: 3, suit: black // 41
+    },
+    {
+        name: "4S", value: 4, suit: black // 42
+    },
+    {
+        name: "5S", value: 5, suit: black // 43
+    },
+    {
+        name: "6S", value: 6, suit: black // 44
+    },
+    {
+        name: "7S", value: 7, suit: black // 45
+    },
+    {
+        name: "8S", value: 8, suit: black // 46
+    },
+    {
+        name: "9S", value: 9, suit: black // 47
+    },
+    {
+        name: "10S", value: 10, suit: black // 48
+    },
+    {
+        name: "JS", value: 11, suit: black // 49
+    },
+    {
+        name: "QS", value: 12, suit: black // 50
+    },
+    {
+        name: "KS", value: 13, suit: black // 51
+    },
+];
